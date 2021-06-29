5b90caffea971dd7496c9426b4de97edcce1d94a
Se <jose.abril4@gmail.com>
2021-06-29 14:33:36 +0800

    Added upgradeable OWnable and context contracts, working deploy and upgrade tests

--- migrations/1624845857_upgrade_adamant_contract.js
+++ migrations/1624845857_upgrade_adamant_contract.js
@@ -1,10 +1,10 @@
 // migrations/MM_upgrade_box_contract.js
-import { upgradeProxy } from '@openzeppelin/truffle-upgrades';
+const { upgradeProxy } = require('@openzeppelin/truffle-upgrades')
 
 const Adamant = artifacts.require('Adamant');
 const AdamantV2 = artifacts.require('AdamantV2');
 
-export default async function (deployer) {
+module.exports = async function (deployer) {
   const existing = await Adamant.deployed();
   const instance = await upgradeProxy(existing.address, AdamantV2, { deployer });
   console.log("Upgraded", instance.address);
--- test/adamant.js
+++ test/adamant.js
@@ -1,15 +1,27 @@
-import { deployProxy } from '@openzeppelin/truffle-upgrades';
+const { deployProxy } = require('@openzeppelin/truffle-upgrades');
 
 const Adamant = artifacts.require("Adamant");
+const AdamantV2 = artifacts.require('AdamantV2');
 
 /*
  * uncomment accounts to access the test accounts made available by the
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("Adamant deployer", function (/* accounts */) {
-  it('deployment works', async () => {
-    const instance = await deployProxy(Adamant, [42]);
-    assert.equal(instance.address.toString(), '42');
+contract("Adamant Deployment", function () {
+  it('Deployment should work', async () => {
+    const instance = await Adamant.deployed();
+    const value = await instance.name();
+    console.log("🚀 ~ file: adamant.js ~ line 15 ~ it ~ value", value)
+ 
+    assert.equal(value, 'Adamant');
+  });
+
+  it('Upgrade Deployment should work', async () => {
+    const instance = await AdamantV2.deployed();
+    const value = await instance.name();
+    console.log("🚀 ~ file: adamant.js ~ line 15 ~ it ~ value v2", value)
+ 
+    assert.equal(value, 'Adamant');
   });
 });
